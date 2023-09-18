b7e8290328f5644801fcecb934f6aa375bf78cae
FaridAA <faridabdul1406@gmail.com>
2023-09-18 11:35:59 +0700

    fix index

--- index.ts
+++ index.ts
@@ -1,12 +1,10 @@
 import net from "net"
 
-const server = net.createServer()
-
-
-server.on("message", (msg) => {
-    const data = msg.toString()
+const server = net.createServer(socket => {
+    socket.on("data", (msg) => {
+        const data = msg.toString()
+    })
 })
-
 
 server.listen(8080, () => {
     console.log("server is running at port 8080")
