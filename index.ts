d2e68e10554a46bffeb8a0308bc6c13783d54f51
FaridAA <faridabdul1406@gmail.com>
2023-09-18 16:06:12 +0700

    change port

--- index.ts
+++ index.ts
@@ -23,6 +23,6 @@ const server = createServer(socket => {
     })
 })
 
-server.listen(8090, () => {
-    console.log("server is running at port 8090")
+server.listen(8080, () => {
+    console.log("server is running at port 8080")
 })
